e4bf8c85033eb72b94abf2ebf162e4668e35cd8f
Tariqul Huda <tariqulhuda6@gmail.com>
2022-07-23 21:25:10 +0600

    All ok

---
+++ Script/script.js
@@ -0,0 +1,20 @@
+const id = document.getElementsByClassName('advice-card-id')[0];
+const txt = document.getElementsByClassName('advice-card-text')[0];
+const button = document.getElementsByClassName('advice-card-dice')[0];
+function addText(text){
+    txt.textContent=`"${text}"`
+}
+function addId(idd){
+    id.textContent=`${idd}`;
+}
+function fetchAdvice(){
+    fetch('https://api.adviceslip.com/advice').then(res=>res.json()).then(data=>{
+        addText(data.slip.advice)
+        addId(data.slip.id)
+    }).catch(err=>{
+        addText(err)
+    })
+}
+
+
+button.addEventListener('click',fetchAdvice)
